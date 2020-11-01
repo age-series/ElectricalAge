70ea1270dbec6329de31c3643182fc6466502ebd
cameron <weinfurt.c@hotmail.com>
2020-10-31 22:02:08 -0400

    getTh optimizations and deduplication

--- src/main/java/mods/eln/sim/mna/process/TransformerInterSystemProcess.java
+++ src/main/java/mods/eln/sim/mna/process/TransformerInterSystemProcess.java
@@ -3,6 +3,7 @@ package mods.eln.sim.mna.process;
 import mods.eln.sim.mna.component.VoltageSource;
 import mods.eln.sim.mna.misc.IRootSystemPreStepProcess;
 import mods.eln.sim.mna.state.State;
+import mods.eln.sim.mna.SubSystem.Th;
 
 public class TransformerInterSystemProcess implements IRootSystemPreStepProcess {
     State aState, bState;
@@ -19,8 +20,8 @@ public class TransformerInterSystemProcess implements IRootSystemPreStepProcess 
 
     @Override
     public void rootSystemPreStepProcess() {
-        Th a = getTh(aState, aVoltgeSource);
-        Th b = getTh(bState, bVoltgeSource);
+        Th a = aState.getSubSystem().getTh(aState, aVoltgeSource);
+        Th b = bState.getSubSystem().getTh(bState, bVoltgeSource);
 
         double aU = (a.U * b.R + ratio * b.U * a.R) / (b.R + ratio * ratio * a.R);
         if (Double.isNaN(aU)) {
@@ -29,38 +30,6 @@ public class TransformerInterSystemProcess implements IRootSystemPreStepProcess 
 
         aVoltgeSource.setU(aU);
         bVoltgeSource.setU(aU * ratio);
-    }
-
-    static class Th {
-        double R, U;
-    }
-
-    Th getTh(State d, VoltageSource voltageSource) {
-        Th th = new Th();
-        double originalU = d.state;
-
-        double aU = 10;
-        voltageSource.setU(aU);
-        double aI = d.getSubSystem().solve(voltageSource.getCurrentState());
-
-        double bU = 5;
-        voltageSource.setU(bU);
-        double bI = d.getSubSystem().solve(voltageSource.getCurrentState());
-
-        double Rth = (aU - bU) / (bI - aI);
-        double Uth;
-        //if (Double.isInfinite(d.Rth)) d.Rth = Double.MAX_VALUE;
-        if (Rth > 10000000000000000000.0 || Rth < 0) {
-            Uth = 0;
-            Rth = 10000000000000000000.0;
-        } else {
-            Uth = aU + Rth * aI;
-        }
-        voltageSource.setU(originalU);
-
-        th.R = Rth;
-        th.U = Uth;
-        return th;
     }
 
     public void setRatio(double ratio) {
