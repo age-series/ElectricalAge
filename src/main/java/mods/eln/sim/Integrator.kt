0af310719ef19abac26bd03e5388d083d783be3a
Jared Dunbar <jrddunbr@ja4.org>
2023-07-04 16:01:12 -0400

    Kotlinized Integrator

--- src/main/java/mods/eln/sim/Integrator.kt
+++ src/main/java/mods/eln/sim/Integrator.kt
@@ -1,11 +1,10 @@
 package mods.eln.sim
 
 import java.util.function.Function
-import kotlin.math.cos
 import kotlin.math.roundToLong
 import kotlin.math.sin
 
-class Integrator {
+class Integrator(private var timeStep: Double) {
     private var integrationPhase = 0
     private var stepsTaken = 0
     private var sample: DoubleArray = doubleArrayOf(0.0, 0.0, 0.0, 0.0, 0.0)
@@ -22,7 +21,7 @@ class Integrator {
         integrations = doubleArrayOf(0.0, 0.0, 0.0, 0.0)
     }
 
-    fun nextStep(nextValue: Double, timeStep: Double): Double {
+    fun nextStep(nextValue: Double): Double {
         var summation = 0.0
         stepsTaken++
         summation += sample[1].also { sample[0] = it } * 7
@@ -38,31 +37,18 @@ class Integrator {
 }
 
 fun main() {
-    val testFunction = Function { xa: Double -> sin(2 * xa) }
-    val actualIntegralFunction = Function { x: Double -> sin(x) * sin(x) }
-    val actualDerivativeFunction = Function { x: Double -> 2 * cos(2 * x) }
+    val testFunction = Function { xa: Double -> sin(10 * xa) }
+    val actualIntegrationFunction = Function { x: Double -> sin(5 * x) * sin(5 * x) / 5 }
     val dx = 1.0 / 20
-    val integrator = Integrator()
-    val differentiator = Differentiator()
+    val integrator = Integrator(dx)
     val tEnd = (1 / dx).roundToLong() + 1
-    for (t in 1 until tEnd) {
+    for (t in 0 until tEnd) {
         val x: Double = t * dx
-        val y = testFunction.apply(x)
-        val derivativeY: Double = differentiator.nextStep(y, dx)
-        val integralY: Double = integrator.nextStep(y, dx)
-        val derivativeActual: Double = actualDerivativeFunction.apply(x)
-        val integralActual: Double = actualIntegralFunction.apply(x)
-        println(
-            "x = %.5f, y = %.5f, Y ~= %.5f, Y = %.5f, dy ~= %.5f, dy = %.5f, errInt = %.3e, errDiff = %.3e".format(
-                x,
-                y,
-                integralY,
-                integralActual,
-                derivativeY,
-                derivativeActual,
-                integralY - integralActual,
-                derivativeY - derivativeActual
-            )
+        val test: Double = integrator.nextStep(testFunction.apply(x))
+        val actual: Double = actualIntegrationFunction.apply(x)
+        val error: Double = test - actual
+        System.out.printf(
+            "x = %1f, Y ~= %2f, Y = %3f, E = %4e%n", x, test, actual, error
         )
     }
 }
