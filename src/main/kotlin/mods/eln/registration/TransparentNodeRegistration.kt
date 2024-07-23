bf69fb385caca8a6843a2a26f9467db8f946b27a
TheBuilderBoy76 <kai.styrsky@gmail.com>
2024-07-22 20:45:25 -0400

    Enable jrddunbr's existing code for floodlights, fix bug causing crash when placing motorized floodlight, add English translations for floodlight names, implement temporary item textures (slightly modified versions of existing texture in model folder), adjust animations (remove sharp jump back to 0 in X)

--- src/main/kotlin/mods/eln/registration/TransparentNodeRegistration.kt
+++ src/main/kotlin/mods/eln/registration/TransparentNodeRegistration.kt
@@ -24,6 +24,9 @@ import mods.eln.misc.Utils.printFunction
 import mods.eln.railroad.OverheadLinesDescriptor
 import mods.eln.sim.ThermalLoadInitializer
 import mods.eln.sim.ThermalLoadInitializerByPowerDrop
+import mods.eln.sixnode.lampsocket.LampSocketDescriptor
+import mods.eln.sixnode.lampsocket.LampSocketStandardObjRender
+import mods.eln.sixnode.lampsocket.LampSocketType
 import mods.eln.sound.SoundCommand
 import mods.eln.transparentnode.*
 import mods.eln.transparentnode.autominer.AutoMinerDescriptor
@@ -133,7 +136,7 @@ object TransparentNodeRegistration {
         registerTransparentNodeMisc(65)
         registerTurret(66)
         registerFuelGenerator(67)
-        //registerFloodlight(68);
+        registerFloodlight(68)
         registerFestive(69)
         registerFab(70)
         registerRailroad(71)
@@ -213,6 +216,25 @@ object TransparentNodeRegistration {
         }
     */
 
+    private fun registerFloodlight(id: Int) {
+        var subId: Int
+        var name: String
+
+        run {
+            subId = 0
+            name = TR_NAME(I18N.Type.NONE, "Basic Floodlight")
+            val desc = BasicFloodlightDescriptor(name, Eln.obj.getObj("Floodlight"))
+            transparentNodeItem.addDescriptor(subId + (id shl 6), desc)
+        }
+
+        run {
+            subId = 1
+            name = TR_NAME(I18N.Type.NONE, "Motorized Floodlight")
+            val desc = MotorizedFloodlightDescriptor(name, Eln.obj.getObj("FloodlightMotor"))
+            transparentNodeItem.addDescriptor(subId + (id shl 6), desc)
+        }
+
+    }
 
     private fun registerLargeRheostat() {
         val id = 96
