32916309a2abd74e6c4fc360d2c3639e67b2e5a2
Jared Dunbar <jrddunbr@ja4.org>
2026-02-23 11:30:05 -0500

    Quick fix for tests reverting introduced bug

--- src/main/kotlin/mods/eln/sim/process/destruct/BipoleVoltageWatchdog.kt
+++ src/main/kotlin/mods/eln/sim/process/destruct/BipoleVoltageWatchdog.kt
@@ -7,7 +7,7 @@ class BipoleVoltageWatchdog(var bipole: Bipole) : ValueWatchdog() {
     fun setNominalVoltage(nominalVoltage: Double): BipoleVoltageWatchdog {
         max = nominalVoltage * 1.3
         min = -max
-        timeoutReset = nominalVoltage
+        timeoutReset = nominalVoltage * 0.5
         return this
     }
 
