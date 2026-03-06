520aa3fb53188c68d745d9c47333b9ad1c7be688
Jared Dunbar <jrddunbr@ja4.org>
2026-03-05 22:59:23 -0500

    Fixed a bunch of WIP pole stuff
    * Added subtransmission poles
    * Added unreleased pole variants
    * Added telecom pole model fix
    * Added UV's to many poles
    * Fixed rotation for distribution poles

---
+++ src/main/java/mods/eln/transparentnode/subtransmissionpole/SubTransmissionPoleCeilingElement.java
@@ -0,0 +1,10 @@
+package mods.eln.transparentnode.subtransmissionpole;
+
+import mods.eln.node.transparent.TransparentNode;
+import mods.eln.node.transparent.TransparentNodeDescriptor;
+
+public class SubTransmissionPoleCeilingElement extends SubTransmissionPoleElementBase {
+    public SubTransmissionPoleCeilingElement(TransparentNode node, TransparentNodeDescriptor descriptor) {
+        super(node, descriptor);
+    }
+}
