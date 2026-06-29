8d746a60b1834fe8fa44f8eb9ea52ce02b701508
Jared Dunbar <jrddunbr@ja4.org>
2026-06-28 23:52:08 -0400

    Split benchmarks from unit tests, add GHA

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,22 @@
 plugins {
     id("com.gtnewhorizons.gtnhconvention")
 }
+
+tasks.named<Test>("test") {
+    exclude("**/*BenchmarkTest.*", "**/*ProfilingTest.*")
+}
+
+tasks.register<Test>("benchmarkTest") {
+    description = "Runs benchmark and profiling tests separately from the regular correctness test suite."
+    group = "verification"
+
+    testClassesDirs = sourceSets.test.get().output.classesDirs
+    classpath = sourceSets.test.get().runtimeClasspath
+
+    include("**/*BenchmarkTest.*", "**/*ProfilingTest.*")
+    shouldRunAfter(tasks.named("test"))
+}
+
+tasks.named("check") {
+    dependsOn(tasks.named("test"))
+}
